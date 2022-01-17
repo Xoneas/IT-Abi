9b8cc71c595843f4f04ac2e95be6402d31f360ba
Xoneas <81169720+Xoneas@users.noreply.github.com>
2022-01-17 11:15:11 +0100

    Update Übung2.java

--- Java/Übung/Übung2.java
+++ Java/Übung/Übung2.java
@@ -5,9 +5,11 @@ public class Name {
   public static main void (String args []){
     boolean nochmal = true
       While (nochmal == true) {
-    Cosole eingabe = console.eingabe;
+    Cosole eingabe = System.console();
       
     
     }
   }
+ System.out.println("Nochmal? Ja=true Nein=false");
+ nochmal = Boolean parseBoolean(eingabe.ReadLine());
 }
