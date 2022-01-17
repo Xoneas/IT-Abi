df450886aae07d4ad0ea04cf378eb1863029af27
Xoneas <81169720+Xoneas@users.noreply.github.com>
2022-01-17 10:24:17 +0100

    Update Motorsteuerung.ino

--- Arduino/Unterprogramme/Motorsteuerung.ino
+++ Arduino/Unterprogramme/Motorsteuerung.ino
@@ -1,40 +1,40 @@
-//Anschlüsse der Motoren rechts
+//Motorenpins
+//Rechter Motor
 int PWM1_L = 3;
 int PWM1_R = 5;
-//Anschlüsse der Motoren links
-int PWM2_R =9;
-int PWM2_L =10;
-//Initialisierung, alle Motor-PINS als Ausgänge 
-void motorInit() { 
-   pinMode(PWM1_L, OUTPUT);
-   pinMode(PWM1_R, OUTPUT);
-   pinMode(PWM2_R, OUTPUT);
-   pinMode(PWM2_L, OUTPUT); 
+//Linker Motor
+int PWM2_L = 10;
+int PWM2_R = 9;
+
+void motorInit() {
+  pinMode(PWM1_L, OUTPUT);
+  pinMode(PWM1_R, OUTPUT);
+  pinMode(PWM2_L, OUTPUT);
+  pinMode(PWM2_R, OUTPUT);
 }
-void setMotorPower(int linkerMotor, int rechterMotor) {
- 
-  if(linkerMotor > 0) {   //linker Motor vorwärts
-  analogWrite(PWM2_R, linkerMotor);
-  digitalWrite(PWM2_L, LOW);
-}
- else if(linkerMotor < 0) { //linker Motor rückwerts
-  analogWrite(PWM2_L, -linkerMotor);
-  digitalWrite(PWM2_R, LOW);
-}
-else { //linker Motor bremst
-  digitalWrite(PWM2_L, HIGH);
-  digitalWrite(PWM2_R, HIGH);
-} 
- if(linkerMotor > 0) {   //linker Motor vorwärts
-  analogWrite(PWM1_L, linkerMotor);
-  digitalWrite(PWM1_R, LOW);
-}
-else if (rechterMotor < 0) { //rechter Motor rückwärts
-  analogWrite(PWM1_R, -rechterMotor);
-  digitalWrite(PWM1_L, LOW);
-  }
- else {
-    digitalWrite(PWM1_L,HIGH);
+
+void setMotorPower(int motorLinks, int motorRechts) {
+  //Rechte Motorsteuerung
+  if (motorRechts > 0) {          //Rechter Motor vorwärts
+    analogWrite(PWM1_R, motorRechts);
+    digitalWrite(PWM1_L, LOW);
+  } else if (motorRechts < 0) {  //Rechter Motor rückwärts
+    analogWrite(PWM1_L, -motorRechts);
+    digitalWrite(PWM1_R, LOW);
+  } else {                        //Rechter Motor bremsen
+    digitalWrite(PWM1_L, HIGH);
     digitalWrite(PWM1_R, HIGH);
-    }
+  }
+
+  //Linke Motorsteuerung
+  if (motorLinks > 0) {           //Linker Motor vorwärts
+    analogWrite(PWM2_R, motorLinks);
+    digitalWrite(PWM2_L, LOW);
+  } else if (motorLinks < 0) {    //Linker Motor rückwärts
+    analogWrite(PWM2_L, -motorLinks);
+    digitalWrite(PWM2_R, LOW);
+  } else {                        //Linker Motor bremsen
+    digitalWrite(PWM2_L, HIGH);
+    digitalWrite(PWM2_R, HIGH);
+  }
 }
