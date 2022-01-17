54cb82ec2efce6ec26a8b81763869033fa7d87dc
Xoneas <81169720+Xoneas@users.noreply.github.com>
2022-01-17 11:13:24 +0100

    Create Übung2.java

---
+++ Java/Übung/Übung2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Name {
+ 
+  public static main void (String args []){
+    boolean nochmal = true
+      While (nochmal == true) {
+    Cosole eingabe = console.eingabe;
+      
+    
+    }
+  }
+}
