fda350d491b22443f3c60d10c8939d54ad79ee31
Xoneas <81169720+Xoneas@users.noreply.github.com>
2022-01-17 10:14:16 +0100

    Update ÜbungBodensensoren.ino

--- Arduino/Übung/ÜbungBodensensoren.ino
+++ Arduino/Übung/ÜbungBodensensoren.ino
@@ -13,3 +13,43 @@ int getBodenwertLinks(){
 int getBodenwertRechts(){
   return analogRead(sensorRechts);
 }
+----------------------------
+
+int SensorLinks = 6;
+int SensorRechts = 7;
+
+void BodenInit () {
+  analogRead(SensorLinks);
+  analogRead(SensorRechts);
+}
+
+int GetBodenwertLinks () {
+  return   analogRead(SensorLinks);
+}
+
+
+int GetBodenwertRechts () {
+  return   analogRead(SensorRechts);
+}
+
+
+----------------------------------------
+
+int SensorLinks = 6;
+int SensorRechts = 7;
+
+void BodenInit () {
+  analogRead(SensorLinks);
+  analogRead(SensorRechts);
+}
+
+
+int getBodenwertLinks () {
+  return   analogRead(SensorLinks);
+}
+
+
+
+int getBodenwertRechts () {
+  return   analogRead(SensorRechts);
+}
