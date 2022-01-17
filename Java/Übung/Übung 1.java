65e684a648215830252dd157b08f2bc582b6cbbe
Xoneas <81169720+Xoneas@users.noreply.github.com>
2022-01-17 08:43:52 +0100

    Update Übung 1.java

--- Java/Übung/Übung 1.java
+++ Java/Übung/Übung 1.java
@@ -16,56 +16,154 @@ System.out.println("Das Ergebnis ist "+ Ergebnis +");
 }
 System.out.println("Wollen sie das Programm wiederholen? Ja=true, Nein=false.")
 nochmal = Boolean.parseBoolean(eingabe.readLine());
-}
+}                
 
 
 
+import java.io.*;
+
+  
+                   
+      public class Test {
+       
+        
+        
+        public static void main(String args[]) {
+        }
+      }
                    
                    
+
+
+
+
+
+
+
+
+import java.io.*;
                    
+                   public class Name {
+                     
+                     
+                     
+                     public static void main (String args []){
+                     }
+                     
+                   }
+
+
+
+
+
                    
+                  import java.io.*;
                    
-                   
-                   
-                   
-                   
-                   
+                   public class Name {
+                     
+                    public static void main (String args[]) {
+                      
+                     boolean nochmal = true
+                       while (nochmal == true) {
+                         
+                         
+                       }
+                      
+                    }
+                     
+System.out.println("Wollen sie das Programm wiederholen? Ja=true, Nein=false.")
+nochmal = Boolean.parseBoolean(eingabe.readLine());
+                   }
+
+
+
+
+
+
                    
                    
                    
                    import java.io.*;
+                   
+                   public class Name {
+                     
+                    public static void main (String args[]) {
+                      
+                      boolean nochmal = true;
+                      while(nochmal == true) {
+                     Console eingabe = Sysem.colsole();
+                     
+                     
+                      }
+                    } 
+                     
+                     
+                     System.out.println("Nochmal? Ja=true Nein=false");
+                     nochmal = Boolean parseBoolean(eingabe.readLine());
+                   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+
+
+
+import java.io.*;
 
 public class Bremsen{
-  
-  public static void main(String args[]){
-     boolean nochmal = true;
-    while (nochmal == true) { 
-      Console eingabe = System.console();
-      System.out.println("Geben Sie Ihren maximalen Bremsweg ein: ");
-      int bremsweg = Integer.parseInt(eingabe.readLine());
-      double a = 4.3;
-      double strecke = 0;
-      double zeit =0;
-      double aKMH = a*3.6;
-      double bremswegKMH = bremsweg*3.6;
-      double maxV = Math.sqrt(bremswegKMH * (2*aKMH));
-      
-      for (int i= 1;i<= (maxV/5) ;i++ ) {
-        double v= 5*i;
-        double vNeu= v/3.6;
-        System.out.print("s: ");
-        strecke = (vNeu*vNeu)/(2*a);
-        double streckeR = Math.round(strecke*100.00)/100.00;
-        System.out.print(streckeR);
-        System.out.print("  ");
-        System.out.print("t: ");
-        zeit = vNeu/a;
-        double zeitR = Math.round(zeit*100.00)/100.00;
-        System.out.println(zeitR); 
-      } // end of for
-      
-      System.out.println("Nochmal? Ja= true, Nein = false");
-      nochmal = Boolean.parseBoolean(eingabe.readLine());
-    } // end of while
-    }
-  }
+
+public static void main(String args[]){
+boolean nochmal = true;
+while (nochmal == true) { 
+Console eingabe = System.console();
+System.out.println("Geben Sie Ihren maximalen Bremsweg ein: ");
+int bremsweg = Integer.parseInt(eingabe.readLine());
+double a = 4.3;
+double strecke = 0;
+double zeit =0;
+double aKMH = a*3.6;
+double bremswegKMH = bremsweg*3.6;
+double maxV = Math.sqrt(bremswegKMH * (2*aKMH));
+
+for (int i= 1;i<= (maxV/5) ;i++ ) {
+double v= 5*i;
+double vNeu= v/3.6;
+System.out.print("s: ");
+strecke = (vNeu*vNeu)/(2*a);
+double streckeR = Math.round(strecke*100.00)/100.00;
+System.out.print(streckeR);
+System.out.print("  ");
+System.out.print("t: ");
+zeit = vNeu/a;
+double zeitR = Math.round(zeit*100.00)/100.00;
+System.out.println(zeitR); 
+} // end of for
+
+System.out.println("Nochmal? Ja= true, Nein = false");
+nochmal = Boolean.parseBoolean(eingabe.readLine());
+} // end of while
+}
+}
