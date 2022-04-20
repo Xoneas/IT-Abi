0c7f0242e377de97cfb03cfc02527fb10159d6e8
Xoneas <81169720+Xoneas@users.noreply.github.com>
2022-04-20 16:56:01 +0200

    Update Odemetire.ino

--- Arduino/Unterprogramme/Odemetire.ino
+++ Arduino/Unterprogramme/Odemetire.ino
@@ -7,8 +7,8 @@ volatile int zaehlerlinks = 0;  //Hilfsvariable für Zählung links
 volatile int zaehlerrechts = 0; //Hilfsvariable für Zählung rechts
 volatile int WS_LEFT = A0;      //Pin für WS left
 volatile int WS_RIGHT = A1;     //Pin für WS Right
-volatile float WegLinksCM=0;
-volatile float WegRechtsCM=0;
+volatile float WegLinksCM = 0;
+volatile float WegRechtsCM = 0;
 
 
 void zaehlFunktionlinks()  //Funktionen fürs hochzählen
@@ -18,27 +18,27 @@ void zaehlFunktionlinks()  //Funktionen fürs hochzählen
 
 void zaehlFunktionrechts()
 {
-  zaehlerrechts ++; 
+  zaehlerrechts ++;
 }
 
 void odoInit()
 {
-  pinMode(WS_LEFT,INPUT_PULLUP);
-  pinMode(WS_RIGHT,INPUT_PULLUP);
+  pinMode(WS_LEFT, INPUT_PULLUP);
+  pinMode(WS_RIGHT, INPUT_PULLUP);
   enableInterrupt(WS_LEFT,  zaehlFunktionlinks, FALLING);    //Falling bedeutet das der Interrupt dann ausgelöst wird wenn der pin von high auf low wechselt
   enableInterrupt(WS_RIGHT, zaehlFunktionrechts, FALLING);  //enableInterrupt wird hier für den Befehl FALLING benötigt
 }
 
 //Werte wiedergeben
 int getInterruptLinks()
-{ 
-  return zaehlerlinks;  
+{
+  return zaehlerlinks;
 }
 
 
 int getInterruptRechts()
 {
-  return zaehlerrechts; 
+  return zaehlerrechts;
 }
 
 void odoReset()
@@ -49,12 +49,12 @@ void odoReset()
 
 float getWegLinksCM()
 {
- WegLinksCM=0.59*zaehlerlinks;
- return WegLinksCM;
+  WegLinksCM = 0.59 * zaehlerlinks;
+  return WegLinksCM;
 }
 
 float getWegRechtsCM()
 {
-  WegRechtsCM=0.59*zaehlerrechts;
+  WegRechtsCM = 0.59 * zaehlerrechts;
   return WegRechtsCM;
 }
